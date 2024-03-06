98daff2333d9951ff89c29018cb56f60894121e8
Peyton Riley <peytonriley9@gmail.com>
2024-03-06 11:06:52 -0600

    Fix orientation of cards in layout

--- Source/TrashCardGame/CardLayout.cpp
+++ Source/TrashCardGame/CardLayout.cpp
@@ -12,36 +12,6 @@ ACardLayout::ACardLayout()
 
 }
 
-void ACardLayout::OnConstruction(const FTransform& Transform)
-{
-    Super::OnConstruction(Transform);
-
-	if (RootComponent->GetNumChildrenComponents() == 0)
-	{
-    	// Create card layout location components
-		for (int i = 1; i <= 10; ++i)
-		{
-			FName ComponentName = FName(*FString::Printf(TEXT("Card%d"), i));
-			// CreateDefaultSubobject<>();
-			ABaseCard* CardComp = NewObject<ABaseCard>(this, ComponentName);
-
-			if (CardComp)
-			{
-				CardComp->AttachToComponent(RootComponent, FAttachmentTransformRules::KeepRelativeTransform);
-			}
-			else
-			{
-				// Failed to create LayoutLocation
-				UE_LOG(LogTemp, Error, TEXT("Failed to create LayoutLocation for Card %d"), i);
-			}
-		}
-	}
-	else 
-	{
-		UE_LOG(LogTemp, Warning, TEXT("There are already card components (%i) in this layout: %s"), RootComponent->GetNumChildrenComponents(), *this->GetHumanReadableName());
-	}
-}
-
 // Called when the game starts or when spawned
 void ACardLayout::BeginPlay()
 {
